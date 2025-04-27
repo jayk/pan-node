da50dcd65f0b6212fbe1438b2dba4db82a040697
Jay Kuri <jayk@ionzero.com>
2025-04-27 15:30:47 -0600

    rename messageTypes to constants, as we have more

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -16,8 +16,24 @@ const VALID_AGENT_MESSAGE_TYPES = [
     'agent_control'
 ];
 
+const NULL_ID = '00000000-0000-0000-0000-000000000000';
+
+function isNullId(id) {
+    return id === NULL_ID;
+}
+
+function isNullFromField(from) {
+    return from &&
+           isNullId(from.node_id) &&
+           isNullId(from.conn_id);
+}
+
+
 module.exports = {
     VALID_CLIENT_MESSAGE_TYPES,
     VALID_PEER_MESSAGE_TYPES,
-    VALID_AGENT_MESSAGE_TYPES
+    VALID_AGENT_MESSAGE_TYPES,
+    NULL_ID,
+    isNullId,
+    isNullFromField
 };
