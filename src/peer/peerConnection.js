bfa760bc3d8c2618bd64d4336746edb9e4c98520
Jay Kuri <jayk@ionzero.com>
2025-04-27 07:45:45 -0600

    Revise PeerConnection to use new validation routines

--- src/peer/peerConnection.js
+++ src/peer/peerConnection.js
@@ -1,22 +1,49 @@
-class peerConnection {
-    constructor(ws, node_id) {
+const { validateIncomingPeerMessage } = require('../utils/validators');
+const { log } = require('../utils/log');
+
+class PeerConnection {
+    constructor(ws, nodeId, router) {
         this.ws = ws;
-        this.node_id = node_id;
+        this.nodeId = nodeId;
+        this.router = router;
 
         ws.on('message', this._onMessage.bind(this));
         ws.on('close', () => {
-            console.log(`[peer] Peer disconnected: ${node_id}`);
-            // deregister if needed
+            log.info(`[peer] Disconnected: ${nodeId}`);
         });
     }
 
     _onMessage(data) {
-        // route peer_control, direct, broadcast
+        let msg;
+        try {
+            msg = JSON.parse(data.toString());
+        } catch (err) {
+            log.warn(`[peer] Invalid JSON from peer ${this.nodeId}`);
+            this.ws.close();
+            return;
+        }
+
+        if (!validateIncomingPeerMessage(msg)) {
+            log.warn(`[peer] Protocol violation from peer ${this.nodeId}: ${errors.join('; ')}`);
+            this.ws.close();
+            return;
+        }
+
+        this.router.handleIncomingMessage(this.nodeId, msg);
     }
 
     sendMessage(msg) {
-        this.ws.send(JSON.stringify(msg));
+        if (this.ws.readyState === this.ws.OPEN) {
+            this.ws.send(JSON.stringify(msg));
+        }
+    }
+
+    close() {
+        if (this.ws.readyState === this.ws.OPEN) {
+            this.ws.close();
+        }
     }
 }
 
-module.exports = { peerConnection };
+module.exports = { PeerConnection };
+
